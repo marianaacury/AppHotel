7ed011595a8ecfc107de192500b0499844b731a3
marianaacury <mariana.cury@aluno.senai.br>
2026-05-07 11:25:24 -0300

    Commit

--- AppHotel/App.xaml.cs
+++ AppHotel/App.xaml.cs
@@ -7,11 +7,18 @@ namespace AppHotel
         public App()
         {
             InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
         }
     }
 }
